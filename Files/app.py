074a8f56d3d151410ea8940bd2c7c88fd122ed96
Barry Far <Bardiafardar@gmail.com>
2023-05-01 08:02:32 +0330

    Added NEW Feature
    Read the releases

--- Files/app.py
+++ Files/app.py
@@ -5,15 +5,7 @@ import os
 
 
 def decode_base64(encoded):
-    """
-    Decode base64-encoded string to text.
 
-    Args:
-        encoded (str): The base64-encoded string to decode.
-
-    Returns:
-        str: The decoded text.
-    """
     decoded = ''
     for encoding in ['utf-8', 'iso-8859-1']:
         try:
@@ -25,15 +17,6 @@ def decode_base64(encoded):
 
 
 def generate_v2ray_configs(decoded_data):
-    """
-    Sorts V2Ray configs in alphabetical order.
-
-    Args:
-        decoded_data (List[str]): A list of V2Ray configs.
-
-    Returns:
-        List[str]: A sorted list of V2Ray configs.
-    """
 
     configs = []
 
@@ -46,15 +29,6 @@ def generate_v2ray_configs(decoded_data):
 
 
 def decode_links(links):
-    """
-    Fetches V2Ray subscription links and decodes the contents.
-
-    Args:
-        links (List[str]): A list of V2Ray subscription links.
-
-    Returns:
-        List[str]: A list of decoded V2Ray configs.
-    """
 
     decoded_data = []
 
@@ -70,15 +44,7 @@ def decode_links(links):
 
 
 def decode_dir_links(dir_links):
-    """
-    Fetches V2Ray config files from URLs and returns their contents.
 
-    Args:
-        dir_links (List[str]): A list of URLs pointing to V2Ray config files.
-
-    Returns:
-        List[str]: A list of V2Ray configs.
-    """
 
     decoded_dir_links = []
 
@@ -107,12 +73,38 @@ def main():
     decoded_dir_links = decode_dir_links(dir_links)
     merged_configs = decoded_links + decoded_dir_links
     output_folder = os.path.abspath(os.path.join(os.getcwd(), '..'))
-    output_file = os.path.join(output_folder, 'configs.txt')
 
+    # Delete existing output files
+    filename = os.path.join(output_folder, f'All_Configs_Sub.txt')
+    if os.path.exists(filename):
+        os.remove(filename)
+    for i in range(20):
+        filename = os.path.join(output_folder, f'Sub{i}.txt')
+        if os.path.exists(filename):
+            os.remove(filename)
+    
+
+    # Write merged configs to output file
+    output_file = os.path.join(output_folder, 'All_Configs_Sub.txt')
     with open(output_file, 'w') as f:
         for config in merged_configs:
             f.write(config + '\n')
 
+    # Split merged configs into files with no more than 1000 configs per file
+    with open(output_file, 'r') as f:
+        lines = f.readlines()
+    num_lines = len(lines)
+    max_lines_per_file = 1000
+    num_files = (num_lines + max_lines_per_file - 1) // max_lines_per_file
+    for i in range(num_files):
+        start_index = i * max_lines_per_file
+        end_index = (i + 1) * max_lines_per_file
+        filename = os.path.join(output_folder, f'Sub{i+1}.txt')
+        with open(filename, 'w') as f:
+            for line in lines[start_index:end_index]:
+                f.write(line)
 
+
+    
 if __name__ == "__main__":
     main()
