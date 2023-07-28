0d5939923700396d1bd1acd9790c73c888f0da40
Barry Far <31126364+Bardiafa@users.noreply.github.com>
2023-07-28 10:38:50 +0330

    Added Donated Sub 🎁❤️

--- Files/app.py
+++ Files/app.py
@@ -65,7 +65,9 @@ def main():
     dir_links = [
         'https://raw.githubusercontent.com/IranianCypherpunks/sub/main/config',
         'https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/sub/sub_merge.txt',
-        'https://raw.githubusercontent.com/yebekhe/TelegramV2rayCollector/main/sub/mix'
+        'https://raw.githubusercontent.com/yebekhe/TelegramV2rayCollector/main/sub/mix',
+        'https://raw.githubusercontent.com/yebekhe/TelegramV2rayCollector/main/sub/donated'
+
     ]
 
     decoded_links = decode_links(links)
